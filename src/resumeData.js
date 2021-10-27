83dc622db0c4ae314974424396f6643f0b3f917e
Hariprasath <54160097+anbuharry@users.noreply.github.com>
2021-10-27 17:17:13 +0530

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,110 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/anbuharry",
+    "name": "Hariprasath",
+    "role": "Python Fullstack Developer",
+    "linkedinId":"https://www.linkedin.com/in/hariprasath-lakshmanan-01b9b8144",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of software development and like to learn about new technologies.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/hariprasath-lakshmanan-01b9b8144",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/anbuharry",
+          "className":"fa fa-github"
+        },
+        
+        
+      ],
+    "aboutme":"My name is Hariprasath , i have more than 2 years Experience‌ ‌in‌ ‌the‌ ‌Software‌ ‌industry‌ ‌with‌ ‌a‌ ‌wide‌ ‌range‌ ‌of‌ ‌experience‌ ‌in‌ ‌IT‌ ‌Services‌. Experienced‌ ‌software‌ ‌engineer‌ ‌with‌ ‌a‌ ‌passion‌ ‌for‌ ‌developing‌ ‌innovative‌ ‌programs‌ ‌that‌ ‌expedite‌ ‌the‌‌ effectiveness‌ ‌of‌ ‌organizational‌ ‌success. Confident‌ ‌communicator,‌ ‌strategic‌ ‌thinker,‌ ‌and‌ ‌innovative‌ ‌creator‌ ‌to‌ ‌develop‌ ‌software‌ ‌and‌ ‌further‌‌ their‌ ‌success.‌",
+    "address":"India",
+    "website":"anbuhari65@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Mahendra Engineering College ,Namakkal",
+        "specialization":"B.E Computer Science and Engineering",
+        "MonthOfPassing":"Apr",
+        "YearOfPassing":"2019",
+        "Achievements":"Won the club activity under the scheme of “WEEK OF HUMANITY” , Represented as Main Coordinator of Innovfest 2k19 symposium conducted by CSE Department"
+        
+      }
+      
+    ],
+    "work":[
+      {
+        "CompanyName":"Cognizant Technology Solutions",
+        "specialization":"Technical Support Engineer",
+        "MonthOfLeaving":"Jun 2019 -",
+        "YearOfLeaving":"Jul 2020",
+        "Achievements":"Handling‌ ‌tickets‌ ‌through‌ ‌BMC‌ ‌Remedy‌ ‌IT‌ ‌service‌ ‌management‌ ‌portal.‌ Installation‌ ‌of‌ ‌Client‌ ‌Operating‌ ‌Systems‌ ‌–‌ ‌windows‌ ‌1709,‌ ‌Windows1809, Thin‌ ‌OS,‌ ‌macOS‌ ‌via‌ ‌WDS‌‌ and‌ ‌USB‌ ‌boot. Software‌ ‌installed‌ ‌through‌ ‌remote‌ ‌tools‌ ‌and‌ ‌maintaining‌ ‌the‌ ‌license‌ ‌for‌ ‌applications.‌‌"
+      },
+      {
+        "CompanyName":"VDA Info Solutions",
+        "specialization":"Software Developer",
+        "MonthOfLeaving":"Aug 2020 -",
+        "YearOfLeaving":"Jan 2021",
+        "Achievements":"Assisting‌ ‌the‌ ‌development‌ ‌manager‌ ‌with‌ ‌all‌ ‌aspects‌ ‌of‌ ‌software‌ ‌design‌ ‌and‌ ‌coding.‌ Monitoring‌ ‌the‌ ‌technical‌ ‌performance‌ ‌of‌ ‌internal‌ ‌systems.‌ ‌Gathering‌ ‌information‌ ‌from‌ ‌consumers‌ ‌about‌ ‌program‌ ‌functionality.‌ ‌"
+      },
+      {
+        "CompanyName":"‌Fraction‌ ‌Analytics‌ ‌Private‌ ‌Limited‌",
+        "specialization":"Full‌ ‌Stack‌ ‌Developer",
+        "MonthOfLeaving":"Feb 2021 -",
+        "YearOfLeaving":"Present ",
+        "Achievements":"Developing‌ ‌Front‌ ‌&‌ ‌Back‌ ‌end‌ ‌website‌ ‌architecture‌ ‌and‌ ‌Ensuring‌ ‌responsiveness‌ ‌of‌ ‌applications.‌ Seeing‌ ‌through‌ ‌a‌ ‌project‌ ‌from‌ ‌conception‌ ‌to‌ ‌finished‌ ‌product.‌ Gathering‌ ‌information‌ ‌from‌ ‌consumers‌ ‌about‌ ‌program‌ ‌functionality."
+      }
+    ],
+    "skillsDescription":"TECHNICAL SKILS :",
+    "skills":[
+      {
+        "skillname":"PYTHON"
+      },
+      {
+        "skillname":"DJANGO"
+      },
+      {
+        "skillname":"ANGULAR , REACT JS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Video summarization",
+        "description":"Generating‌ ‌a‌ ‌short‌ ‌video‌ ‌with‌ ‌informative‌ ‌material‌ ‌of‌ ‌a‌ ‌longer‌ cctv‌ ‌video",
+        "imgurl":"images/fvsumm.jpeg"
+      },
+      {
+        "name":"Anomaly detection in surveillance",
+        "description":"Identifies‌‌ ‌events ‌that‌ ‌deviate‌ ‌from‌ ‌a‌ ‌‌normal‌ ‌behavior.‌",
+        "imgurl":"images/anomaly.jpeg"
+      },
+      {
+        "name":"Future invoice",
+        "description":"smart invoice scanner",  
+        "imgurl":"images/invoice.jpeg"
+      },
+      {
+        "name":"Resume parser",
+        "description":"Analyze most important details from resume",  
+        "imgurl":"images/resume.jpeg"
+      },
+      
+    ],
+    "testimonials":[
+      {
+        "description":"Strive always to excel in virtue and truth.",
+        "name":"Prophet Muhammad"
+      },
+      {
+        "description":"Concentrate On Your Goal Do Your Work And Focus Not On The Result.",
+        "name":"Bhagavad Gita"
+      },
+      {
+        "description":"Whatever you do, work at it with all your heart, as working for the Lord, not for human masters,",
+        "name":"The Bible"
+      }
+    ]
+  }
+  
+  export default resumeData
