1167591ae3aa83e75a894f0c1f2b53e958ddd740
anbuharry <anbuhari65@gmail.com>
2021-10-27 17:34:07 +0530

    Updates

---
+++ precache-manifest.046403c7cabb6e66e298e1d4e6875956.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fe88872038a18ead8bd",
+    "url": "/My_website/static/css/main.9b05a465.chunk.css"
+  },
+  {
+    "revision": "4fe88872038a18ead8bd",
+    "url": "/My_website/static/js/main.4fe88872.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/My_website/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "7ce971784df02ad5b8c6",
+    "url": "/My_website/static/js/runtime~main.7ce97178.js"
+  },
+  {
+    "revision": "a2c23e3391110869af54497c3968d5a5",
+    "url": "/My_website/index.html"
+  }
+];
